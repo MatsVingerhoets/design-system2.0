1cfa892df59cdbc4bbc0a9dd0aff3855b1f12130
MatsVingerhoets <mats.vingerhoets5@gmail.com>
2025-04-03 09:26:00 +0200

    changed atom to components docs

--- docs/assets/Card.stories-dSCUy09n.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{C as o}from"./Card-3i61ckYc.js";import"./clsx-B-dksMZM.js";const m={title:"Atoms/Card",component:o,tags:["autodocs"]},t={args:{title:"Dit is een titel",children:r.jsx("p",{className:"text-black",children:"test"})}};var e,s,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    title: "Dit is een titel",
-    children: <p className='text-black'>test</p>
-  }
-}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const p=["Default"];export{t as Default,p as __namedExportsOrder,m as default};
