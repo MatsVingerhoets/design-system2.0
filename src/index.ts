43e399ce4ae34bd9e9c44de328d01c35466c0b22
MatsVingerhoets <mats.vingerhoets5@gmail.com>
2025-04-03 08:20:17 +0200

    deploy storybook

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { default as Button } from "./components/Button/Button";
+export { default as Card } from "./components/Card/Card";
+export { default as Spinner } from "./components/Spinner/Spinner";
+
+export { default as ClientCard } from "./molecules/ClientCard/ClientCard";
